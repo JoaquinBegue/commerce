353de0cc73740b52ec50571065a8cd91293eaffa
Joaquín Begué <joaquinbegue1204@gmail.com>
2023-12-18 17:45:32 -0300

    Changed name of 'auctions' db table to 'listing'. Also added livereload.

--- auctions/models.py
+++ auctions/models.py
@@ -6,14 +6,14 @@ class User(AbstractUser):
     pass
 
 
-class Auction(m.Model):
+class Listing(m.Model):
     title = m.CharField(max_length=64)
     description = m.TextField()
     price = m.FloatField()
     categ = m.ForeignKey('Category', on_delete=m.SET_NULL, blank=True, null=True)
     date = m.DateTimeField(auto_now_add=True)
     image_url = m.TextField(blank=True)
-    creator = m.ForeignKey(User, related_name="auctions", on_delete=m.CASCADE)
+    creator = m.ForeignKey(User, related_name="listings", on_delete=m.CASCADE)
     winner = m.ForeignKey(User, related_name="won", on_delete=m.SET_NULL, blank=True, null=True)
 
     def __str__(self):
@@ -22,26 +22,26 @@ class Auction(m.Model):
 
 class Bid(m.Model):
     amount = m.FloatField()
-    auction = m.ForeignKey(Auction, related_name='bids', on_delete=m.CASCADE)
+    listing = m.ForeignKey(Listing, related_name='bids', on_delete=m.CASCADE)
     author = m.ForeignKey(User, related_name='bids', on_delete=m.CASCADE)
 
     def __str__(self):
-        return f"({self.id}) {self.author} bids ${self.amount} for {self.auction}"
+        return f"({self.id}) {self.author} bids ${self.amount} for {self.listing}"
 
 
 class Comment(m.Model):
     text = m.TextField()
-    auction = m.ForeignKey(Auction, related_name='comments', on_delete=m.CASCADE)
+    listing = m.ForeignKey(Listing, related_name='comments', on_delete=m.CASCADE)
     author = m.ForeignKey(User, related_name='comments', on_delete=m.SET_NULL, null=True)
     date = m.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"({self.id}) Author: {self.author} about {self.auction}"
+        return f"({self.id}) Author: {self.author} about {self.listing}"
 
 
 class Category(m.Model):
     name = m.CharField(max_length=30)
-    auctions = m.ManyToManyField(Auction, blank=True)
+    listings = m.ManyToManyField(Listing, blank=True)
 
     def __str__(self):
         return f"({self.id}) {self.name}"
@@ -49,7 +49,7 @@ class Category(m.Model):
 
 class Watchlist(m.Model):
     owner = m.ForeignKey(User, related_name='watchlist', on_delete=m.CASCADE)
-    auctions = m.ManyToManyField(Auction, related_name='watchlist', blank=True)
+    listings = m.ManyToManyField(Listing, related_name='watchlist', blank=True)
 
     def __str__(self):
         return f"({self.id}) {self.owner}"
